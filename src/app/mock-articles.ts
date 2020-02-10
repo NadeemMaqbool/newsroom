50d85b142f062aa6645cc051d130b30b8a78da4e
Nadeem <nadeem@technolife.ee>
2020-02-10 14:27:10 +0200

    New Angular Blog first commit with components created and Html Template added

---
+++ src/app/mock-articles.ts
@@ -0,0 +1,25 @@
+import { Article } from './article';
+
+export const ARTICLES: Article[] = [
+      {
+            id: 1,
+            title: 'New features will add to dashboard soon',
+            content: '',
+            description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+            key: 'new-feature-in-dashboard',
+            imageUrl: 'https://via.placeholder.com/300/09f/fff.png',
+            date: new Date()
+
+      },
+      {
+            id: 2,
+            title: 'Download our Chrome extension',
+            content: '',
+            description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+            key: 'download-our-extension',
+            imageUrl: 'https://via.placeholder.com/300',
+            date: new Date()
+
+      }
+
+];
